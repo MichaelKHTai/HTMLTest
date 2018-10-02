d6fb9cc08ba989ef43565a0a1b74269199ce8e78
MichaelKHTai <kuihontai@gmail.com>
2018-10-02 16:18:23 +0800

    Update

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
-define(function (require) {
-    var module1 = require("./module");
-    console.log(module1())
 
-    // Rest of the module.
-});
+var module1 = require("./module");
+console.log(module1())
+alert(module1())
+var a = 5
+console.log(5)
