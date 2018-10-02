0d04e1deaaadd0d492011857fc2dce47a03077e1
MichaelKHTai <kuihontai@gmail.com>
2018-10-02 17:54:02 +0800

    Update

--- client.js
+++ client.js
@@ -1,3 +1,8 @@
-requirejs(['module'], function (module1) {
+require.config({
+    paths: {
+        module: 'module'
+    }
+});
+require(['module'], function (module1) {
     console.log(module1())
 });
