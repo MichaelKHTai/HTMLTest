725d94e051c10383083c1926781b861c04a109b0
MichaelKHTai <kuihontai@gmail.com>
2018-10-02 15:59:47 +0800

    Update

--- client.js
+++ client.js
@@ -1,3 +1,6 @@
 import require;
-var module1 = require('./module');
+var moduleName = 'module';
+module1 = require([moduleName], function(fooModule){
+    // do something with fooModule
+})
 console.log(module1());  // prints: "this is module1! and this is module2!"
